99e06e95d744800c6f9f3143d43a2ebb1a6921a1
frenale59 <gregoire.adias@gmail.com>
2021-11-30 21:20:56 +0100

    Revert "Merge branch 'greg' into test"
    This reverts commit 1f068d2ec07c0e8edc221a0535f7f85aed42dcc6, reversing
    changes made to 387d33ce04d03c239ff80e27537ff28800701b7c.

--- Spline-main/Assets/Scripts/Player/TargetShooter.cs
+++ Spline-main/Assets/Scripts/Player/TargetShooter.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
-using Unity;
-using System.Collections;
-using System.Collections.Generic;
 
 public class TargetShooter : MonoBehaviour
 {
     #region Serialize Fields
     [SerializeField] Camera cam;
-
-    [SerializeField] public ParticleSystem ParticleSystem;
-
-    [SerializeField] public AudioSource GunSound;
     #endregion
 
     #region API
@@ -18,11 +11,6 @@ public class TargetShooter : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GunSound.Stop();
-            GunSound.Play();
-            ParticleSystem.Play();
-
-
             Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
             if(Physics.Raycast(ray, out RaycastHit hit))
             {
